e6c7aa8160920f578ce321feccaa5498fe848ee8
Juha Tiihonen <tiihonenjm@ornl.gov>
2020-12-03 16:50:57 -0500

    Cleanup, update coronene example and templates

--- template/nexus/run_ls.py
+++ template/nexus/run_ls.py
@@ -1,28 +1,33 @@
 #! /usr/bin/env python3
 
-from parameters import *
-from nexus import settings,run_project
+from nexus import settings,run_project,obj
 from surrogate import IterationData,surrogate_diagnostics
 
-from run_phonon import FC_param as hessian
-from run_phonon import R_relax  as pos_init
+from parameters import pos_to_params,params_to_pos,get_prime_jobs,nx_settings
 
+from run_phonon import hessian
+from run_phonon import pos_relax
+from run_error_scan import data as srg_error
+
+n_max = 3 # number of iterations
 ls_settings = obj(
-    get_jobs      = get_jobs
+    get_jobs      = get_prime_jobs
     pos_to_params = pos_to_params,
-    pfn           = 4,
-    S             = 7,
-    epsilon       = 0.01,
+    type          = 'scf',
+    load_postfix  = '/scf.in',
+    path          = 'scf/',
+    pfn           = srg_data.pfn,
+    pts           = srg_data.pts,
+    epsilon       = srg_data.epsilon,
+    targets       = pos_to_params(pos_relax)
     )
 data_ls = [] # first iteration
 
-# first iteration, try to load data
-data = IterationData(n=0, **ls_settings)
-data.load_pos(pos_init)
-data.load_hessian(hessian)
-data.shift_positions()
+# first iteration
+data = IterationData(n=0, pos=pos_init, hessian=hessian, **ls_settings)
 data_load = data.load_from_file()
 if data_load is None:
+    data.shift_positions()
     settings(**nx_settings)
     jobs = data.get_job_list()
     run_project(jobs)
@@ -33,21 +38,22 @@ else:
 #end if
 data_ls.append(data)
 
-# second iteration
-data = data.iterate(ls_settings=ls_settings,divide_epsilon=2)
-data_load = data.load_from_file()
-if data_load is None:
-    settings(**nx_settings)
-    jobs = data.get_job_list()
-    run_project(jobs)
-    data.load_results()
-    data.write_to_file()
-else:
-    data = data_load
-#end if
-data_ls.append(data)
-
-# third iteration, etc
+# repeat to n_max iterations
+for n in range(1,n_max):
+    data = data.iterate(ls_settings=ls_settings)
+    data_load = data.load_from_file()
+    if data_load is None:
+        data.shift_positions()
+        settings(**nx_settings)
+        jobs = data.get_job_list()
+        run_project(jobs)
+        data.load_results()
+        data.write_to_file()
+    else:
+        data = data_load
+    #end if
+    data_ls.append(data)
+#end for
 
 if __name__=='__main__':
     surrogate_diagnostics(data_ls)
